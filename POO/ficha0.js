3eb0e0ded3fdd20aaf25c635b8f5d382e60a3cf7
clara10mota-cpu <clara10mota@gmail.com>
2026-03-23 14:13:33 +0000

    .

--- POO/ficha0.js
+++ POO/ficha0.js
@@ -1,41 +1,44 @@
 
-let nome = "teclado Mecânico";
+let nome = "Teclado mecânico";
 let preço = 89.99;
-let quantidade = 14;
+let stockDisponivel = 14;
 let promoção = true;
 let desconto = undefined;
 
+/* iva  */
 
-let iva = preço * 0.23;
+let iva = preço * 1.23; 
 
-let preçoFinal = preço + iva;
+console.log(`${nome} - ${iva.toFixed(2)} (stock : ${stockDisponivel})`);
 
-console.log(`${nome} - ${preçoFinal.toFixed(2)} (${quantidade})`);
+function classificarStock (stockDisponivel) {
 
-function classificarStock (quantidade) {
     switch (true) {
-        case quantidade === 0:
+        case stockDisponivel === 0 :
             return "Esgotado";
-            case quantidade >=1 && quantidade <= 5:
-                return "Stock critico";
-                case quantidade >6 && quantidade <= 20:
-                    return "Stock normal";
-                    case quantidade > 20:
-                        return "Stock elevado";
-
+            case stockDisponivel >= 1 && stockDisponivel <= 5 :
+                return "Stock crítico";
+                case stockDisponivel >= 6 && stockDisponivel <= 20 :
+                    return "stock normal"
+                    case stockDisponivel > 20 :
+                        return "Stock elevado"
 
     }
 }
 
+// testes //
+
+console.log(classificarStock(0));
 console.log(classificarStock(4));
-console.log(classificarStock(10));
-console.log(classificarStock(99));
-console.log(classificarStock(1));
+console.log(classificarStock(7));
+console.log(classificarStock(30));
 
+let emPromocao = promoção ? "Em promoção" : "Preço normal" ;
 
-let etiqueta = promoção ? "Em promoção" : "preço normal";
+// testes //
 
-console.log(etiqueta);
+console.log(`${emPromocao}`);
+
 
 const produtos = [
   {
@@ -75,139 +78,9 @@ const produtos = [
   },
 ];
 
-produtos.forEach(produtos => {
+produtos.forEach (produtos => {
     console.log(produtos.nome.toUpperCase());
 })
 
-function calcularValorStock (produtos) {
-   
-    let valorTotal = 0;
-    produtos.forEach(produtos => {
-
-        let valorStock = produtos.preco * produtos.stock;
-        valorTotal += valorStock;
-
-    })
-    return valorTotal;
-
-
-}
-
-console.log("O valor total em stock é: " + calcularValorStock(produtos));
-
-produtosDisponiveis = produtos.filter(produtos => produtos.stock > 0);
-
-produtosIndisponiveis = produtos.filter(produtos => produtos.stock === 0);
-
-console.log(produtosDisponiveis);
-console.log(produtosIndisponiveis);
-
-
-function descricaoProduto (produtos) {
-  let quantidade = classificarStock(produtos.stock);
-  let etiqueta = produtos.promoção ? "Em promoção" : "preço normal";
-  return `${produtos.nome} - ${produtos.preco} - ${etiqueta} - ${quantidade}`;
-}
-
-produtos.forEach(produtos => {
-  console.log(descricaoProduto (produtos));
-})
-
-
-
- function renderizarProdutos(produtos) {
-
-  const container = document.getElementById("lista-produtos");
-
-   container.innerHTML = "";
-
-  produtos.forEach(produtos => {
-    const div = document.createElement("div");
-    div.textContent = descricaoProduto(produtos);
-    container.appendChild(div);
-
-  })
-}
-
-renderizarProdutos(produtos);
-
-let disponiveis = false;
-
-const botao = document.getElementById("btn-disponiveis");
-
-botao.addEventListener("click",() => {
-  if (disponiveis === false){
-    const disponiveis = produtos.filter(produtos => produtos.stock > 0);
-    renderizarProdutos(disponiveis);
-    botao.textContent ="Mostrar todos ";
-    disponiveis = true;
-  } else {
-    renderizarProdutos(produtos);
-    botao.textContent ="Mostrar disponiveis";
-    disponiveis = false;
-  }
-
-})
-
-/* desafio */
-
-function ordenarPorPreco(produtos) {
-  return [...produtos].sort((a, b) => a.preco - b.preco);
-}
-
-renderizarProdutos(ordenarPorPreco(produtos));
-
-const inputPesquisa = document.getElementById("pesquisa");
-
-inputPesquisa.addEventListener("input", function () {
-  const texto = inputPesquisa.value.toLowerCase();
-
-  const produtosFiltrados = produtos.filter((produto) =>
-    produto.nome.toLowerCase().includes(texto)
-  );
-
-  renderizarProdutos(ordenarPorPreco(produtosFiltrados));
-});
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* calcular o valor do stock */
 
